41908164f6e1237ee78da0c93d5c8e9f9fa78af0
José Carlos <josecarlos.lopezayala@gmail.com>
2019-09-18 17:53:08 +0200

    app tutorial

--- tutorial/createXLDTutorialResources.py
+++ tutorial/createXLDTutorialResources.py
@@ -1,9 +1,11 @@
 # Este script se invoca de forma remota para crear la infraestructura de la aplicación de Tutorial
 # La invocación se debe realizar de la siguiente forma:
 # ./cli.sh -f /home/jcla/Projects/xld-scripts/tutorial/createXLDTutorialResources.py
-# El usuario y la password están en el fichero /opt/xebialabs/xl-deploy-9.0.3-cli/conf/deployit.conf con el siguiente contenido
+# El usuario y la password están en el fichero /opt/xebialabs/xl-deploy-X-cli/conf/deployit.conf con el siguiente contenido
 # cli.username=admin
 # cli.password=password      <- la password se modifica la primera vez que ejecutemos el cli
+
+# La aplicación la cargamos utilizando xl apply -f app.yaml
 
 def createResource(name, type, props):
     if not repository.exists(name):
@@ -16,8 +18,23 @@ def createResource(name, type, props):
     else:
         print("CI {0} existed".format(name))
 
+def createOrUpdateDictionary(env):
+    dictionaryName = "Environments/dictionaries-tutorial/dictionary-application-tutorial-{0}".format(env)
+    if not repository.exists(dictionaryName):
+        myDict = factory.configurationItem(dictionaryName, 'udm.Dictionary', {})
+        repository.create(myDict)
+        print("Dictionary {0} created".format(dictionaryName))
+    else:
+        myDict = repository.read(dictionaryName)
+        # Por ahora no necesitamos dictEntries
+        # myDict.entries = dictEntries
+        repository.update(myDict)
+        print("Dictionary {0} updated".format(dictionaryName))
+
 def createOrUpdateEnvironment(env):
     environmentName = "Environments/application-tutorial/application-tutorial-{0}/application-tutorial-{0}".format(env)
+    dictionaryName = "Environments/dictionaries-tutorial/dictionary-application-tutorial-{0}".format(env)
+    dictionaries = [dictionaryName]
     requiresOkTestManager = False
     requiresOkReleaseManager = False
     if env == "pre":
@@ -26,7 +43,7 @@ def createOrUpdateEnvironment(env):
         requiresOkTestManager = True
         requiresOkReleaseManager = True
     myContainers = [
-        "Infrastructure/K8s/cluster-k8s-local/ns-tutorial-{0}".format(env)
+        "Infrastructure/KubernetesClusters/on-premise-cluster/ns-tutorial-{0}".format(env)
     ]
     # Calculamos los triggers
     if env == "dev":
@@ -39,12 +56,13 @@ def createOrUpdateEnvironment(env):
     triggerSuccessful = "Configuration/Environments/{0}/Triggers/SuccessfulDeploymentTrigger".format(triggerEnvironmentName)
     triggers = [triggerFailed, triggerSuccessful]
     if not repository.exists(environmentName):
-        myEnvironment = factory.configurationItem(environmentName, 'udm.Environment', {'members': myContainers, 'requiresOkTestManager': requiresOkTestManager, 'requiresOkReleaseManager': requiresOkReleaseManager, 'triggers': triggers})
+        myEnvironment = factory.configurationItem(environmentName, 'udm.Environment', {'members': myContainers, 'dictionaries': dictionaries, 'requiresOkTestManager': requiresOkTestManager, 'requiresOkReleaseManager': requiresOkReleaseManager, 'triggers': triggers})
         repository.create(myEnvironment)
         print("Environment {0} created".format(environmentName))
     else:
         myEnvironment = repository.read(environmentName)
         myEnvironment.members = myContainers
+        myEnvironment.dictionaries = dictionaries
         myEnvironment.requiresOkTestManager = requiresOkTestManager
         myEnvironment.requiresOkReleaseManager = requiresOkReleaseManager
         myEnvironment.triggers = triggers
@@ -56,31 +74,42 @@ def createOrUpdateEnvironment(env):
 #        | |__| (_) | | | | || (_| | | | | |  __/ |  \__ \
 #         \____\___/|_| |_|\__\__,_|_|_| |_|\___|_|  |___/
 
-createResource("Infrastructure/K8s", "core.Directory", None)
-caCert = '''
------BEGIN CERTIFICATE-----
+createResource("Infrastructure/KubernetesClusters", "core.Directory", None)
+caCertOnPremise = '''
+----BEGIN CERTIFICATE-----
 MIICyDCCAbCgAwIBAgIBADANBgkqhkiG9w0BAQsFADAVMRMwEQYDVQQDEwprdWJl
-cm5ldGVzMB4XDTE5MDgwNjE0MzI1OFoXDTI5MDgwMzE0MzI1OFowFTETMBEGA1UE
-AxMKa3ViZXJuZXRlczCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMUx
-sCAFuq6XwS1j/mVxD60Waehr4Lb4Eg3OqSsxH6Ev+vNn7daOYleqQkqRdvF9NEKQ
-47YBxA+2hDbf5ZpvKYsHWIy3Eq23fehs3rr3XRmrJcNM8PbwBEY93nyunZQTYZQI
-EuFQ8OeHNYTJ2qR07kCAWdzqyjcCPzNVrE5+8zZlDrOLPl7ubVnEykDTYyFSp2ly
-vjHAQ+wQlnUvsuChvfXXydnAQy6AuuTgLrdMEdHWD7JC4+qV6YSk6j6EB2mhuJKF
-nJJ/E1BkQgeYS5hFrbDq15427YBe+hipoOM2ePmbxCjoHH9iXcGfVMhjAidtpQrv
-dYBkUb4TgnZsPQ8xuxkCAwEAAaMjMCEwDgYDVR0PAQH/BAQDAgKkMA8GA1UdEwEB
-/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAILAFTT6dUu7ZvgxAXkm9s2q7BDo
-LbgrtfIAt9Ir5PjkCLJG0VE5j22U7SaySKeQiseTo3OrvojFASwlIHzCg6kQPkCq
-24sr0OxfXxDDiV1AmZPFsihLcLGcb/dpJqJV1c0eWr++YwryyU90KJBRVpOVvAY8
-uC6mioeNMWrxYk0sZBszgsfZxqJ2b5i9e63s+suQFvofRDkh0O/j5Gj+u6moKpb8
-CKEAax7d1s/yaSrok4qzvig4DltQMAD2fw9iFqPX3A5cQ5aJWb9a4dGJ8e9oUIFO
-Zbv7nI/ilFbCpe+ThouhPTb9LKv+UKpZy/1Y/1ZheiFMlzwr5Stl/wlYdiI=
+cm5ldGVzMB4XDTE5MDkxNzIxMjU1NVoXDTI5MDkxNDIxMjU1NVowFTETMBEGA1UE
+AxMKa3ViZXJuZXRlczCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAPzi
+ufuhXF/vEN6fod9pImBm2zgIJbUXuKuCErTaPx3WA+tjtvldfy8txVnIob8I0opc
+7BESPBOtidqi1ikSn9dPKCNUg1aeDnzXlY8YJLGMI7I9SGR9FmwPWbb2PsOvpxFp
+IEwW2dXlBRThfp8A5EARaOW69OArri+kioZq8SlQtRg7cxUq8gegYDt5RO4X1K9N
+JKq/+XC3dYosC6OksfBtRh1Ym6yRGKAaCku5qDSOfyJikJ6eLNGwvdbvfknJZBU+
+OAu5VqlIqrM+F+8MRutZAsQKMTburbTI60zFPUeo/gKSioUQSG/e13oniT1HUWV+
+KFd4lI8bfFI6wz0F7xMCAwEAAaMjMCEwDgYDVR0PAQH/BAQDAgKkMA8GA1UdEwEB
+/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAKaY2iWrsvMi90050G3fEmGjSK0y
+n5m8u4d8++YBlaxbPL76C+RkuLzAyO2hM+aa0WGhbvULLHF8XB3Sy0HkSx4H64rb
+JzBr6DH5Y1PxqMe/VvSCKV5GpbBbASm4u9UajGvppEF3sZLjsY/aUGuD5MVWs2sW
+o9veu+ATVVz8voH/w0nPsv9/Chkz7htvAHcqapaQxAbW3oBSPhurTtSjlAI2NPmm
+78s/lzzMQ/7L4q4kShJucHsPFOKlUIt7g4Z1MOzWj206DZCxwLC7cr2U1e7tLPao
+OrxxG4DNBvsj0kTOVrhOPo3ZnqVRRamRwXEwfgx+fdp5PproIH6pwao9DK4=
 -----END CERTIFICATE-----
 '''
-token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLWhsZmY2Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiI4ZmMyNDFkNy04MDc1LTRmMzUtYTczZC01MDJmMzA2ODY2ODMiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZS1zeXN0ZW06YWRtaW4tdXNlciJ9.XOa3IU-nxJqklvu6v8nF3su0maX2R9zeybwUfAi5c0nCcDlEt7vavOI3LGk5Fu_5l_P5WAbD2_zIqfiT5911FKtzeqfy4ScSHB-e78mNgq0RXV8Q4oXWs9dgh8Wb90v-4Q57JbukKVWILTaf3r_E8j3M_7cF3jbXo3ubewqW09DEycrYwDl2bJOiPhYXUmuJFWKHAvVeRwQPOsM7y9HDMoI2OFSvHo-5ecCI4rAfsPHEQRRamAb2VnM2e-XSGB7fKnniR-Acyxch0bVelpT69QVPpvuv-h60ZZlPYvzqj8rF-jdCipm1-IeOugRRwQ7AH-N-a5Vdii1zFHLrCLYhHQ'
-createResource("Infrastructure/K8s/cluster-k8s-local", "k8s.Master", {'apiServerURL': 'https://172.42.42.100:6443', 'caCert': caCert, 'token': token})
-createResource("Infrastructure/K8s/cluster-k8s-local/ns-tutorial-dev", "k8s.Namespace", {'namespaceName': 'ns-tutorial-dev'})
-createResource("Infrastructure/K8s/cluster-k8s-local/ns-tutorial-pre", "k8s.Namespace", {'namespaceName': 'ns-tutorial-pre'})
-createResource("Infrastructure/K8s/cluster-k8s-local/ns-tutorial-pro", "k8s.Namespace", {'namespaceName': 'ns-tutorial-pro'})
+tokenOnPremise = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLThwNmNxIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiI4NGIzNzAwZC0wOTY4LTQxMzMtYmM1ZC1jZmQzZTE3ZjA0N2UiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZS1zeXN0ZW06YWRtaW4tdXNlciJ9.eG1Mjen1JPo--zGjK65DfO0J9JTty0O5nZphkKaqfIR9Hzkr7tYBwzzihQmHXy1-yjSPD-n2bQG6SyKWfSYkyc8D0j_Ozjl-wViw0aKcFzz0lAq6SGYQ1ipco-pUbrN_hnnrgEX2UsxaRXvRYNqj-d0TUg4hahr17WuYgsolHgVlPaaLd8-zN1p3vt5AsdfixKxfgtiRCt9JM0V8ibRmkfzo6GamrZr8ASXm44iQH5Dn1PxypZRggtXSEZ7_PkYf4Ap9FP6kC01r1mMOl77VytzY2QtVE8O7ij0TSOFgzXcT2AMsi0th2vCkSSYOcACedbcy2l10z1jd3nBtKDGj_Q'
+createResource("Infrastructure/KubernetesClusters/on-premise-cluster", "k8s.Master", {'apiServerURL': 'https://172.42.42.100:6443', 'skipTLS': True, 'caCert': caCertOnPremise, 'token': tokenOnPremise})
+createResource("Infrastructure/KubernetesClusters/on-premise-cluster/ns-tutorial-dev", "k8s.Namespace", {'namespaceName': 'ns-tutorial-dev'})
+createResource("Infrastructure/KubernetesClusters/on-premise-cluster/ns-tutorial-pre", "k8s.Namespace", {'namespaceName': 'ns-tutorial-pre'})
+createResource("Infrastructure/KubernetesClusters/on-premise-cluster/ns-tutorial-pro", "k8s.Namespace", {'namespaceName': 'ns-tutorial-pro'})
+
+
+# |  _ \(_) ___| |_(_) ___  _ __   __ _ _ __(_) ___  ___ 
+# | | | | |/ __| __| |/ _ \| '_ \ / _` | '__| |/ _ \/ __|
+# | |_| | | (__| |_| | (_) | | | | (_| | |  | |  __/\__ \
+# |____/|_|\___|\__|_|\___/|_| |_|\__,_|_|  |_|\___||___/
+
+createResource("Environments/dictionaries-tutorial", "core.Directory", None)
+createOrUpdateDictionary('dev')
+createOrUpdateDictionary('pre')
+createOrUpdateDictionary('pro')
 
 # | ____|_ ____   _(_)_ __ ___  _ __  _ __ ___   ___ _ __ | |_ ___ 
 # |  _| | '_ \ \ / / | '__/ _ \| '_ \| '_ ` _ \ / _ \ '_ \| __/ __|
