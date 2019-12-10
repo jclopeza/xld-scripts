858a87bda94e467dd8f1f46efd58dbc9ef438a07
José Carlos <josecarlos.lopezayala@gmail.com>
2019-12-10 18:00:40 +0100

    adecuamos creacion con datos del cluster

--- voting-app-k8s/createXLDVotingAppK8sResources.py
+++ voting-app-k8s/createXLDVotingAppK8sResources.py
@@ -76,6 +76,28 @@ def createOrUpdateEnvironment(env):
 #        | |__| (_) | | | | || (_| | | | | |  __/ |  \__ \
 #         \____\___/|_| |_|\__\__,_|_|_| |_|\___|_|  |___/
 
+createResource("Infrastructure/KubernetesClusters", "core.Directory", None)
+caCertOnPremise = '''
+-----BEGIN CERTIFICATE-----
+MIICyDCCAbCgAwIBAgIBADANBgkqhkiG9w0BAQsFADAVMRMwEQYDVQQDEwprdWJl
+cm5ldGVzMB4XDTE5MDkyNDE2NTEwN1oXDTI5MDkyMTE2NTEwN1owFTETMBEGA1UE
+AxMKa3ViZXJuZXRlczCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAJpc
+e6EOgIjCyPIzGu85KQMNGrshoXelpW+kQ1Vh3KOg4YUTfE7JI8Yk1c0IUuZ4XBEo
+kiDIEW7LkmXM7biTcWoFrnp+yn8YjWsZ4x1ZpcmptyxTZqiwZKRN8Hz7WrWiVKhE
+s+0/+u/onQAr1gs0+it0YG1nrrxK9PW5opeMtyl3pU8ol5RaKU99xVxGdeJtxt5o
+C6K/jP4HY3C4c5GmBIRa2UOSSyz2g3//AfHymrZjPTzfA8lBoPtC2zp6ImZI9ehp
+z4rptkEOtOjhnyk55+Q5nhnMHKmOw8u/6FNovdotZaprcQn/cQQHyzCp15kyaWwv
+NKBcXOWPfHqJ/Y34S+UCAwEAAaMjMCEwDgYDVR0PAQH/BAQDAgKkMA8GA1UdEwEB
+/wQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAFanPd/OwmZ0EcE4y2RFhByguAMw
+yYrdr90Hf10NKdg+logU0NFFbeGECDcINwgdLQzT4K/rb9sASAlEiCZHRnfSWLzS
+hBj+ikWM4ipBPVAnP/jkDW32I3+GchatHriA6madlpL8B2Jbk0gwsqIEYtNFhFR1
+Gg7nrEIvLMKtQ//9Bt+gI6+hykMASK1wlWmBIcaee5aMvFyTz7Z4BQPDC3uA7wsG
+t/Gus1LOSq+ZXsbTUoB5Lnh4oyqxXdrwWPHdL3jQ3NBcxVxlROVnz5p831UEEKPd
+Y7UzIOvpvWh4xUbiscw4CuCCY0h7F7Fn6xUnO6fDO1LyqLWEQhJCjv0lFGA=
+-----END CERTIFICATE-----
+'''
+tokenOnPremise = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IlFWMTFVNWM4UnYxTnZpT3VPOERraUJ6S2dIX182NUZnT2VtSWRvYWpLVTAifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLWZ0OGJ0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiIxZGU3NTQ0Ny1lN2I0LTRjOWQtYjRkYi0wMjM5ZjY5ODBkZDAiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZS1zeXN0ZW06YWRtaW4tdXNlciJ9.miLhUMyVBSfSpUS4tcDzR4tmmArKVHOsBNapDSTFiramNUg0aPoAhvoLIgMtXDesQlkPh9s-hnFR0LlM1FfVvAwCCP1UHip_jDAt2oLrdbMaB-TgBLlUO5VmW0aQVDRhqztg6mADfl14_O5lnI-cjU5Pq37rdHv_7H8nEk7CA1XhgwH77jROs19JUwmmmZM3CrG8M00jQHx6E1pC5zVxN_joG1F3Hm9q5Ngk0ZrGh7l9sS9NZrhY4riX5ZjLsC29SkJIA0Hc59Uhjiv8yShUnBsOC4PKAiCltFWTlvE38g6cvIOf4j7YJ5KgVhiXeLyOYFQs8f8UJYE-xmdUu2WPrg'
+createResource("Infrastructure/KubernetesClusters/on-premise-cluster", "k8s.Master", {'apiServerURL': 'https://172.42.42.100:6443', 'skipTLS': True, 'caCert': caCertOnPremise, 'token': tokenOnPremise})
 createResource("Infrastructure/KubernetesClusters/on-premise-cluster/ns-voting-app-dev", "k8s.Namespace", {'namespaceName': 'ns-voting-app-dev'})
 createResource("Infrastructure/KubernetesClusters/on-premise-cluster/ns-voting-app-pre", "k8s.Namespace", {'namespaceName': 'ns-voting-app-pre'})
 createResource("Infrastructure/KubernetesClusters/on-premise-cluster/ns-voting-app-pro", "k8s.Namespace", {'namespaceName': 'ns-voting-app-pro'})
